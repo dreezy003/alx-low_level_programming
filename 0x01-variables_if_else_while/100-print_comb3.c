5f8a2b3fcddaf68de9e4d5ea52a013983a81f329
dreezy003 <idrisbalogun36@gmail.com>
2023-06-16 21:51:01 +0000

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,14 +7,14 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 19; i <= 34; i++)
+	for (i = 19; i < 34; i++)
 	{
 		for (j = 20; j <= 35; j++)
 		{
 			if (j > i)
 			{
-				putchar(i);
-				putchar(j);
+				putchar(i + '0');
+				putchar(j + '0');
 				if (i != 34 ||  j != 35)
 				{
 					putchar(',');
